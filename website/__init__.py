a8fb4d943e0c0fb2611a8f65264d20083d912e3f
AakashSuresh2003 <aakashsuresh455@gmail.com>
2025-03-12 15:22:25 +0530

    text file update

--- website/__init__.py
+++ website/__init__.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from flask_migrate import Migrate  # Import Flask-Migrate for database migrations
 
-# Database and app initialization
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -13,15 +11,17 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To avoid warnings
-    
     db.init_app(app)
 
     # Register blueprints for views and auth
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    # Create the database if not exists
+    create_database(app)
 
     # Initialize the login manager
     login_manager = LoginManager()
@@ -30,11 +30,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        from .models import User
         return User.query.get(int(id))
-    migrate = Migrate(app, db)
-
-    create_database(app)
 
     return app
 
@@ -45,5 +41,3 @@ def create_database(app):
         with app.app_context():  # Make sure to use app context when creating the DB
             db.create_all()  # Create all tables
         print('Created Database!')
-    else:
-        print("Database already exists, skipping creation.")
